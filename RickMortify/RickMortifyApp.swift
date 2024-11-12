b174f928af5f3b87a0bb3b53bcb843c287f0dae5
Rodrigo Limpias <>
2024-11-12 15:24:31 +0100

    Initial commit

---
+++ RickMortify/RickMortifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickMortifyApp.swift
+//  RickMortify
+//
+//  Created by Rodrigo Limpias Cossio on 12/11/24.
+//
+
+import SwiftUI
+
+@main
+struct RickMortifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
